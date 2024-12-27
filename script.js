a59f5de542bce51443835278f714c766417e2e67
Ly meng <hort.lymeng@institute.pse.ngo>
2024-12-27 16:14:16 +0700

    add js

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ let age = 20;
 let isStudent = true;
 let sibling = 4;
 // Making a sentence by using these variable in console: I’m Jam Sangka, I’m 20 years old, am I a student: true and I have 4 siblings.
-
+console.log(
+  "I'm " +
+    name +
+    " I'm " +
+    age +
+    " year old" +
+    " am I a student " +
+    isStudent +
+    "and I have " +
+    sibling +
+    " sibling."
+);
 // we have a=8, b=4 make division of a and b and show the result in console.
 let a = 8;
 let b = 4;
-
+console.log(8 / 4);
 // show type of this data in console.
 let fullName = "Chan Leakna";
 let num = 123;
 let boolean = true;
-
+console.log;
 // Voting eligibility
 // - Console "Eligible to vote" if the age is 18 or above (hint: you can use age >= 18).
 // - Console "Not eligible to vote" if the age is below 18.
@@ -23,4 +34,4 @@ let personAge = 18;
 //show in console
 let x = 5;
 let y = 2;
-let z = a % b;
+let z = h % y;
