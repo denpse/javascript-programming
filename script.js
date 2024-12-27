95083bfee77da44db4e6d04f346aa1df41eced18
sovanden <sovanden@sovandens-MacBook-Pro.local>
2024-12-27 15:04:27 +0700

    finish programming

---
+++ script.js
@@ -0,0 +1,26 @@
+let name = "Jam Sangka";
+let age = 20;
+let isStudent = true;
+let sibling = 4;
+// Making a sentence by using these variable in console: I’m Jam Sangka, I’m 20 years old, am I a student: true and I have 4 siblings.
+
+// we have a=8, b=4 make division of a and b and show the result in console.
+let a = 8;
+let b = 4;
+
+// show type of this data in console.
+let fullName = "Chan Leakna";
+let num = 123;
+let boolean = true;
+
+// Voting eligibility
+// - Console "Eligible to vote" if the age is 18 or above (hint: you can use age >= 18).
+// - Console "Not eligible to vote" if the age is below 18.
+let personAge = 18;
+// answer here
+
+// What the result of z and why?
+//show in console
+let x = 5;
+let y = 2;
+let z = a % b;
