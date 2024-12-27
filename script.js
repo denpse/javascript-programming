e2287b987dae23125e772102b2c5bb780642167e
mary <thoeun.mary@institute.pse.ngo>
2024-12-27 16:13:34 +0700

    add

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-let name = "Jam Sangka";
-let age = 20;
-let isStudent = true;
-let sibling = 4;
+let name = "I'm Jam Sangka";
+let age = "I'm 20 years old";
+let isStudent = "am I a student : true";
+let sibling = "I have 4 siblings";
+console.log(name, age, isStudent, sibling);
 // Making a sentence by using these variable in console: I’m Jam Sangka, I’m 20 years old, am I a student: true and I have 4 siblings.
 
-// we have a=8, b=4 make division of a and b and show the result in console.
-let a = 8;
-let b = 4;
+// we have a=8, b=4 make division of a and b and show the result in console
 
 // show type of this data in console.
+let a = 8;
+let b = 4;
+console.log(a / b);
+
 let fullName = "Chan Leakna";
 let num = 123;
 let boolean = true;
-
+console.log(typeof fullName);
 // Voting eligibility
 // - Console "Eligible to vote" if the age is 18 or above (hint: you can use age >= 18).
-// - Console "Not eligible to vote" if the age is below 18.
+// - Console "Not eligiblevote" if the age  to is below 18.
 let personAge = 18;
 // answer here
-
+if (personAge >= 18) {
+  console.log("Eligible to vote");
+} else {
+  console.log("Not eligible to vote");
+}
 // What the result of z and why?
 //show in console
 let x = 5;
 let y = 2;
-let z = a % b;
+console.log(x + y);
