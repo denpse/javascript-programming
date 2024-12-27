c53bb739ade3d14537006596a0dc67c09dc882ad
John Doe <johndoe@example.com>
2024-12-27 16:13:46 +0700

    nikev

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ let name = "Jam Sangka";
 let age = 20;
 let isStudent = true;
 let sibling = 4;
+console.log "I'm"+name+"I'm"+age+"am I a student:"+isStudent+"and I have"+sibling+"sibling.";
 // Making a sentence by using these variable in console: I’m Jam Sangka, I’m 20 years old, am I a student: true and I have 4 siblings.
 
 // we have a=8, b=4 make division of a and b and show the result in console.
 let a = 8;
 let b = 4;
+console.log(a/b);
 
 // show type of this data in console.
 let fullName = "Chan Leakna";
 let num = 123;
 let boolean = true;
-
+console.log(typeof(fullName))
+console.log(typeof(num))
+console.log(typeof(boolean))
 // Voting eligibility
 // - Console "Eligible to vote" if the age is 18 or above (hint: you can use age >= 18).
 // - Console "Not eligible to vote" if the age is below 18.
