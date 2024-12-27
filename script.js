4023caab51b44cdf90e66bfef3a20d5c2f3522ec
Roeurnvanneth <roeurn.vanneth@institute.pse.ngo>
2024-12-27 16:09:38 +0700

    neth

--- script.js
+++ script.js
@@ -7,20 +7,29 @@ let sibling = 4;
 // we have a=8, b=4 make division of a and b and show the result in console.
 let a = 8;
 let b = 4;
-
+console.log =(a/b);
 // show type of this data in console.
 let fullName = "Chan Leakna";
 let num = 123;
 let boolean = true;
+console.log  (typeof fullName);
+console.log  (typeof num);
+console.log  (typeof boolean);
 
 // Voting eligibility
 // - Console "Eligible to vote" if the age is 18 or above (hint: you can use age >= 18).
 // - Console "Not eligible to vote" if the age is below 18.
 let personAge = 18;
+if (personAge >=18){
+    console.log("Eligible to vote");
+} else{
+    console.log ("Not eligible to vote");
+}
 // answer here
 
 // What the result of z and why?
 //show in console
 let x = 5;
 let y = 2;
-let z = a % b;
+let z = x % y;
+console.log=(z);
